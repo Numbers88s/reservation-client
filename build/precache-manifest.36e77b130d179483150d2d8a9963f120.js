43ab50f530a568fbabc30e2d7bd5d312dd724da2
Cesar Jimenez <cesar.r.jimenez@gmail.com>
2019-11-18 21:39:55 -0800

    uploading build and retriggering netlify

---
+++ build/precache-manifest.36e77b130d179483150d2d8a9963f120.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1372f7960ff0c4e798c157b465051e6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e67d2c795363f7270e6",
+    "url": "/static/css/2.c6231af7.chunk.css"
+  },
+  {
+    "revision": "3c6927053e4a31b80c57",
+    "url": "/static/css/main.890d0dd4.chunk.css"
+  },
+  {
+    "revision": "6e67d2c795363f7270e6",
+    "url": "/static/js/2.2a4e0080.chunk.js"
+  },
+  {
+    "revision": "3c6927053e4a31b80c57",
+    "url": "/static/js/main.ba9b4ca4.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
